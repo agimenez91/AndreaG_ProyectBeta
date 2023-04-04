8a99a577f8422c18db7f143117a2a73b0ca55e63
agimenez91 <andrea.gimenez91@gmail.com>
2023-04-04 12:28:06 +0200

    actualización 4 abril

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 const hamburger = document.querySelector('.burger');
-const navMenu = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
 const topbar = document.querySelector('.topbar');
 
 hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
+  menu.classList.toggle('active');
   hamburger.classList.toggle('active');
   topbar.classList.toggle('active');
 });
 
-navMenu.addEventListener('click', () => {
-  navMenu.classList.remove('active');
+menu.addEventListener('click', () => {
+  menu.classList.remove('active');
   hamburger.classList.remove('active');
   topbar.classList.remove('active');
 });
-  
